85d4647dac4ebe62830366f6bd4101081d21ae64
deepak <dpsingh0804@gmail.com>
2025-06-24 23:23:19 +0530

    install json server and update environment url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl:'https://reqres.in/api/', 
+  apiUrl: 'https://reqres.in/api/',
+  jsonUrl: 'http://localhost:3000/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false, 
   apiUrl:'https://reqres.in/api/', 
+  jsonUrl:'http://localhost:3000/'
 };
